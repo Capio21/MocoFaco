e76a095feaa81318f286e833e064f3e7ebf3025d
Capio21 <130652957+Capio21@users.noreply.github.com>
2023-11-19 16:20:58 +0800

    frontend

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,6 +6,10 @@ class Home extends BaseController
 {
     public function index(): string
     {
-        return view('welcome_message');
+        return view('User');
+    }
+    public function workshop(): string
+    {
+        return view('WorkShop');
     }
 }
---
+++ app/Views/WorkShop.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <title>Simply Amazed HTML Template by Tooplate</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css" />
+    <link rel="stylesheet" href="fontawesome/css/all.min.css" type="text/css" /> 
+    <link rel="stylesheet" href="css/slick.css" type="text/css" />   
+    <link rel="stylesheet" href="css/tooplate-simply-amazed.css" type="text/css" />
+    <!-- Tooplate 2123 Simply Amazed https://www.tooplate.com/view/2123-simply-amazed -->
+</head>
+
+<body>
+    <div id="outer">
+        <header class="header order-last" id="tm-header">
+            <nav class="navbar">
+                <div class="collapse navbar-collapse">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link" href="/"><span class="icn"><i class="fas fa-2x fa-air-freshener"></i></span> Our Company</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#section-2"><span class="icn"><i class="fab fa-2x fa-battle-net"></i></span> Our Work</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#section-3"><span class="icn"><i class="far fa-2x fa-images"></i></span> Gallery</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#section-4"><span class="icn"><i class="far fa-2x fa-comments"></i></span> Contact</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/workshop"><span class="icn"><i class="fa fa-briefcase fa-2x "></i></span> Work Shop</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+        <section class="gallery-section section parallax-window" data-parallax="scroll" data-image-src="img/section-3-bg.jpg" id="section-3">
+                <div class="container">
+                    <div class="title text-left">
+                        <h2>Our Gallery</h2>
+                    </div>
+                    <div class="mx-auto gallery-slider">
+                        <figure class="effect-julia item">
+                            <img src="img/gallery-img-01.jpg" alt="Image">
+                            <figcaption>
+                                <div>
+                                    <p>Coconut Planting</p>
+                                </div>
+                                <a href="#">View more</a>
+                            </figcaption>
+                        </figure>
+                        <figure class="effect-julia item">
+                            <img src="img/gallery-img-02.jpg" alt="Image">
+                            <figcaption>
+                                <div>
+                                    <p>Coconut Product WorkShop</p>
+                                </div>
+                                <a href="#">View more</a>
+                            </figcaption>
+                        </figure>
+                        <figure class="effect-julia item">
+                            <img src="img/gallery-img-03.jpg" alt="Image">
+                            <figcaption>
+                                <div>
+                                    <p>Meetings for all of members</p>
+                                </div>
+                                <a href="#">View more</a>
+                            </figcaption>
+                        </figure>
+                        <figure class="effect-julia item">
+                            <img src="img/gallery-img-04.jpg" alt="Image">
+                            <figcaption>
+                                <div>
+                                    <p></p>
+                                </div>
+                                <a href="#">View more</a>
+                            </figcaption>
+                        </figure>
+                        <figure class="effect-julia item">
+                            <img src="img/gallery-img-05.jpg" alt="Image">
+                            <figcaption>
+                                <div>
+                                    <p>Julia dances in the deep dark</p>
+                                </div>
+                                
+                            </figcaption>
+                        </figure>
+                       
+                    </div>
+                </div>
+            </section>
+        
+        
+        <button class="navbar-button collapsed" type="button">
+            <span class="menu_icon">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </span>
+        </button>
+    </div>
+
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/bootstrap.bundle.min.js"></script>
+    <script src="js/jquery.singlePageNav.min.js"></script>
+    <script src="js/slick.js"></script>
+    <script src="js/parallax.min.js"></script>
+    <script src="js/templatemo-script.js"></script>
+</body>
+</html>
